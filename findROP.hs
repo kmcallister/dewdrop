59f765e311abe3d9732589124cf235277938019f
Keegan McAllister <mcallister.keegan@gmail.com>
2011-08-19 14:32:14 -0400

    refactor

--- findROP.hs
+++ findROP.hs
@@ -1,11 +1,12 @@
-import qualified Data.ByteString as B
-import System.Environment (getArgs)
-import Control.Monad (when)
-import Data.Word (Word64)
-import Numeric (showHex)
-import Data.List (intercalate, sortBy)
-import Data.Function (on)
+import System.Environment
+import Control.Monad
+import Control.Applicative
+import Data.Word
+import Numeric
+import Data.List
+
 import qualified Data.Set as S
+import qualified Data.ByteString as B
 
 import Data.Elf
 import Hdis86
@@ -25,31 +26,27 @@ toGadget (C addr bytes) = disassembleMetadata cfg $ bytes
         cfg = amd64 { cfgOrigin = addr
                     , cfgSyntax = SyntaxATT }
 
+maxCandidate :: Int
 maxCandidate = 20
 
-flagSections :: ElfSectionFlags -> Elf -> [ElfSection]
-flagSections flag elf = [s | s <- elfSections elf,
-                                  elem flag $ elfSectionFlags s]
-
 execSections :: Elf -> [ElfSection]
-execSections = flagSections SHF_EXECINSTR
+execSections = filter ((SHF_EXECINSTR `elem`) . elfSectionFlags) . elfSections
 
-findROP elf = let exec = execSections elf in concatMap findROPOne exec
-
-findROPOne sect = filter valid $ candidates sect
+findROP :: Elf -> [Candidate]
+findROP = concatMap (filter valid . candidates) . execSections
 
 valid :: Candidate -> Bool
 valid (C _ bytes) =
-    let insns = disassemble amd64 bytes
-    in case insns of
-           []  -> False
-           [_] -> False
-           _ | any invalid insns -> False
-           _ -> case (last $ insns) of
-                    (Inst _ Iret _) -> True
-                    _               -> False
+    case insns of
+        []  -> False
+        [_] -> False
+        _ | any invalid insns -> False
+        _ -> case last insns of
+                 (Inst _ Iret _) -> True
+                 _               -> False
 
     where
+        insns = disassemble amd64 bytes
         invalid (Inst _ Iinvalid _) = True
         invalid _                   = False
 
@@ -57,29 +54,28 @@ candidates :: ElfSection -> [Candidate]
 candidates sect =
     do let bytes = elfSectionData sect
        index <- B.elemIndices 0xC3 $ bytes
-       hd    <- return $ B.take (index + 1) bytes
-       seq   <- B.tails $ B.drop (B.length hd - maxCandidate) hd
+       let hd = B.take (index + 1) bytes
+       sq    <- B.tails $ B.drop (B.length hd - maxCandidate) hd
        return $ C (elfSectionAddr sect +
                    fromIntegral index -
-                   (fromIntegral $ B.length seq)) seq
+                   (fromIntegral $ B.length sq)) sq
 
 formatOne :: Gadget -> String
-formatOne g = showHex addr $ (":\n" ++) $
-              intercalate "\n" $
-              map (("  "++) . mdAssembly) g
-    where
-        cfg  = amd64 { cfgOrigin = addr
-                     , cfgSyntax = SyntaxATT }
-        addr = mdOffset $ head g
+formatOne g@(g1:_)
+    = showHex (mdOffset g1) $ (":\n" ++) $
+      intercalate "\n" $
+      map (("  "++) . mdAssembly) g
+formatOne [] = error "empty gadget"
 
 dedup :: [Candidate] -> [Candidate]
-dedup cs = go cs S.empty
+dedup xs = go xs S.empty
     where
         go [] _ = []
         go (c:cs) seen
             | (candidateAddress c) `S.member` seen = go cs seen
             | otherwise  = c : go cs (S.insert (candidateAddress c) seen)
 
+wanted :: [Metadata] -> Bool
 wanted g = all (noStack . mdInst) g && any (rsi . mdInst) g
     -- Customize this to filter for gadgets that have some property
     -- you want
@@ -89,22 +85,21 @@ wanted g = all (noStack . mdInst) g && any (rsi . mdInst) g
         noStack  (Inst _ _ operands) = all (not . usesReg RSP) operands
         usesReg  r (Reg rr)   = usesReg' r rr
         usesReg  r (Mem (Memory _ base index _ _)) = usesReg' r base || usesReg' r index
-        usesReg  r _          = False
+        usesReg  _ _          = False
         usesReg' r (Reg8  rr _)   = r == rr
         usesReg' r (Reg16 rr)     = r == rr
         usesReg' r (Reg32 rr)     = r == rr
         usesReg' r (Reg64 rr)     = r == rr
-        usesReg' r _              = False
+        usesReg' _ _              = False
         usesMemReg  r (Mem (Memory _ base index _ _)) = usesReg' r base || usesReg' r index
-        usesMemReg  r _           = False
+        usesMemReg  _ _           = False
         rsi (Inst _ _ operands)   = any (usesMemReg RDI) operands
 
 main :: IO ()
 main =
-    do args <- getArgs
-       when (length args < 1) $
-            fail "Usage: findROP ELF-FILE"
-       contents <- B.readFile $ head args
-       elf <- return $ parseElf contents
-       gadgets <- return $ filter wanted $ map toGadget $ dedup $ findROP elf
+    do args@(~[elf_file]) <- getArgs
+       when (null args) $
+           error "Usage: findROP ELF-FILE"
+       elf <- parseElf <$> B.readFile elf_file
+       let gadgets = filter wanted . map toGadget . dedup . findROP $ elf
        mapM_ (putStrLn . formatOne) $ gadgets
